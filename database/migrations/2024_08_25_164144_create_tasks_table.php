775ce83e277daf90f47bc6b51341619a6166b36a
Carlos Andres Balaguera Caicedo <carlos.balagueradev@gmail.com>
2024-08-25 12:06:05 -0500

    Set up migrations, models, and JWT authentication configuration

---
+++ database/migrations/2024_08_25_164144_create_tasks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->date('due_date');
+            $table->enum('status', ['pendiente', 'en progreso', 'completada'])->default('pendiente');
+            $table->enum('priority', ['baja', 'media', 'alta'])->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');  // Relación con la tabla users
+            $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
